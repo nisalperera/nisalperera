d5654a0e828a563d0323acd6655067d1226b80d6
Nisal Perera <chinthanapereranisal@gmail.com>
2025-05-26 12:59:37 +0530

    new updated and data addition

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <img src="./2H8A6958 no back.png" alt="Nisal Perera" />
       </div>
       <h1>Nisal Perera</h1>
-      <p>Computer Vision | Robotic | Research and Development</p>
+      <p>Computer Vision | Robotics | Research and Development</p>
       <p>
         Negombo, Sri Lanka | +94 (77) 3122382 | chinthanapereranisal@gmail.com |{" "}
         <a href="https://www.linkedin.com/in/nisalperera/">LinkedIn</a>
