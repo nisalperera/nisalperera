df69860d7c2fed940179e9d62e8dd4811114a71b
Nisal Perera <nisal.perera@veracitygp.com>
2025-10-15 12:46:39 +0530

    config bug fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const REACT_APP_DEV = process.env.REACT_APP_DEV === "true" || "false";
+export const REACT_APP_DEV = process.env.REACT_APP_DEV === "true" || false;
 export const REACT_APP_DEV_URL = process.env.REACT_APP_DEV_URL || "http://localhost:3000";
 export const REACT_APP_BASE_URL = REACT_APP_DEV ? `${REACT_APP_DEV_URL}/` : "https://nisalperera.github.io/nisalperera/";
